789244b52717791ce7d53abeda42abee9f527cfa
Raghav6786 <kumaraghav079@gmail.com>
2025-04-20 12:17:34 +0530

    Added theme engine. Handles themes and custom styles.

---
+++ vortexui/test.py
@@ -0,0 +1,39 @@
+from theme_engine import ThemeEngine
+from windows import FMainWindow
+from PySide6.QtWidgets import QApplication, QScrollArea, QLabel, QWidget
+from PySide6.QtGui import QFont
+
+
+class Window(FMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Theme Engine Test")
+        self.setMinimumSize(800, 600)
+        self.theme_engine = ThemeEngine()
+    
+        
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setObjectName("scrollArea")
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(
+            QLabel(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            )
+        )
+        self.layout.addWidget(self.scroll_area)
+
+        self.setStyleSheet(
+            self.theme_engine.active_scheme(
+                self.theme_engine.default_theme
+            )
+        )
+        self.theme_engine.get_scroll_areas(self)
+
+
+app = QApplication()
+font = QFont("./fonts/Rajdhani-Regular.ttf")
+app.setFont(font)
+win = Window()
+win.show()
+app.exec()
